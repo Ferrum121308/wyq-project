8424e612db4f8cfadcfeed16466fe71b579bc041
Ferrum <3132099790@qq.com>
2022-04-30 23:54:33 +0800

    第一版完整

---
+++ src/views/DemoVideo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      };
+    },
+    methods: {},
+    components: {},
+    props: {},
+    watch: {},
+    computed: {},
+    created() {},
+    mounted() {}
+  };
+</script>
+<style  scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mainPage">
+    <div class="mainPage" v-if="!otherVideoVisable">
+      <video :src="startVideoUrl" autoplay loop @click="handleClickStart" height="700px"></video>
+    </div>
+    <div class="mainPage" v-if="otherVideoVisable">
+      <video :src="videoUrl" autoplay @ended="handleVideoEnd" height="700px">其他视频</video>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      otherVideoVisable: false,
+      startVideoUrl: '../../video/0.mp4',
+      videoUrl: '',
+      videoNum: 0,
+    }
+  },
+  methods:{
+    handleClickStart(){
+      console.log("调用handleClickStart方法");
+      this.createRandomNum();
+      console.log(this.videoNum);
+      this.videoUrl = '../../video/' + this.videoNum +'.mp4'
+      this.otherVideoVisable = true;
+    },
+    handleVideoEnd(){
+      console.log("测试视频结束")
+      this.otherVideoVisable = false;
+    },
+    createRandomNum(){
+      this.videoNum = Math.floor(Math.random() * 48 + 1);
+    },
   }
 }
 </script>
+
+<style scoped>
+  .mainPage{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .end{
+    margin-top: 300px
+  }
+</style>
